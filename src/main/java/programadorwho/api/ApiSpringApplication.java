b7d809469b8b22f29f990a9db4030ec91cda3e2a
Thiago <thiagoandrecardoso@icloud.com>
2019-10-28 22:18:38 -0300

    created and used variable in application.properties

--- src/main/java/programadorwho/api/ApiSpringApplication.java
+++ src/main/java/programadorwho/api/ApiSpringApplication.java
@@ -1,14 +1,28 @@
 package programadorwho.api;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiSpringApplication {
 
+	@Value("${valor_maximo}")
+	private int valor_maximo;
+
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+
+		return args -> {
+			System.out.println("Valor maximo " + this.valor_maximo);
+		};
+
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(ApiSpringApplication.class, args);
-		System.out.println("Test build travis - 3");
 	}
 
 }
