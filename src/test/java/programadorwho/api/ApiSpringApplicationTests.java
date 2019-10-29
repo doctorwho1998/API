2cbd6860e9680f34f51c5a852438a1c43b8d5072
Thiago <thiagoandrecardoso@icloud.com>
2019-10-28 22:48:16 -0300

    added new profile and your test

--- src/test/java/programadorwho/api/ApiSpringApplicationTests.java
+++ src/test/java/programadorwho/api/ApiSpringApplicationTests.java
@@ -1,13 +1,21 @@
 package programadorwho.api;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ApiSpringApplicationTests {
+
+	@Value("${valor_maximo}")
+	private int valor_maximo;
 
 	@Test
 	void contextLoads() {
+		assertEquals(50, this.valor_maximo); 
 	}
 
 }
